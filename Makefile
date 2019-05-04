3693d531133c5a0a527bf9e00fac2c3b6a77e03c
Ubuntu Rootkit Demo <kiddo@ubuntu>
2019-05-04 10:21:59 -0700

    Script formal en conjunto con su makefile para su uso posterior de algunas acciones extra

---
+++ Makefile
@@ -0,0 +1,8 @@
+
+
+install-cp:
+	cp readme_g.sh /usr/local/bin/greadme
+install-ln:
+	ln readme_g.sh /usr/local/bin/greadme
+clean:
+	rm /usr/local/bin/readme
---
+++ readme_g.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+##Comando para generar plantilla de README.md
+TITULO=$1
+DESCRIPCION=$2
+INSTALACION=$3
+LICENCIA=$4
+AUTOR=$5
+if [ -z $1 ];then
+	read -p "Titulo README :" TITULO
+fi
+if [ -z $2 ];then
+	read -p "Descripcion:" DESCRIPCION
+fi
+if [ -z $3 ];then
+	read -p "Instalacion:" INSTALACION
+fi
+if [ -z $4 ];then
+	read -p "Licencia:" LICENCIA
+fi
+if [ -z $5 ];then
+	read -p "Autor:" AUTOR
+fi
+CONTENT="# $TITULO\n
+\n
+## Descripcion\n
+$DESCRIPCION
+\n
+## Instalacion\n
+$INSTALACION
+\n
+## Licencia\n
+$LICENCIA
+\n
+## Autor\n
+$AUTOR
+\n
+\n
+$(date +'%d-%m-%y')"
+
+echo $CONTENT >> README.md
